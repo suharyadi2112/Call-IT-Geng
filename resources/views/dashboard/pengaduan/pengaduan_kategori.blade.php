c11640ffc6134e5289e79634e6f051de6ca10eda
MSI\USER <yogafebrianmaulana12@gmail.com>
2024-03-13 09:18:59 +0700

    no message

--- resources/views/dashboard/pengaduan/pengaduan_kategori.blade.php
+++ resources/views/dashboard/pengaduan/pengaduan_kategori.blade.php
@@ -10,9 +10,9 @@
                         <div class="d-flex align-items-center">
                             <h4 class="card-title">Daftar Kategori</h4>
                             <div class="ml-auto">
-                                <a href="" class="btn btn-sm btn-success">
+                                <button type="button" class="btn btn-sm btn-success" id="alert_demo_5">
                                     <i class="fa fa-plus"></i> Tambah Kategori
-                                </a>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -30,7 +30,7 @@
                                         <tr>
 
                                             <td>{{ $loop->iteration }}</td>
-                                            <td>{{$m->nama}}</td>
+                                            <td>{{ $m->nama }}</td>
                                         </tr>
                                     @endforeach
                                 </tbody>
@@ -48,6 +48,57 @@
     <script>
         $(document).ready(function() {
             $('#basic-datatables').DataTable({});
+        });
+    </script>
+    <script>
+        //== Class definition
+        var SweetAlert2Demo = function() {
+
+            //== Demos
+            var initDemos = function() {
+                //== Sweetalert Demo 1
+                $('#alert_demo_5').click(function(e) {
+                    swal({
+                        title: 'Nama Kategori',
+                        html: '<br><input class="form-control" name="nama" placeholder="Nama Kategori" id="input-field">',
+                        content: {
+                            element: "input",
+                            attributes: {
+                                placeholder: "Nama Kategori",
+                                type: "text",
+                                id: "input-field",
+                                name:"nama",
+                                className: "form-control"
+                            },
+                        },
+                        buttons: {
+                            cancel: {
+                                visible: true,
+                                className: 'btn btn-danger'
+                            },
+                            confirm: {
+                                className: 'btn btn-success'
+                            }
+                        },
+                    }).then(
+                        function() {
+                            swal("", "You entered : " + $('#input-field').val(), "success");
+                        }
+                    );
+                });
+            };
+
+            return {
+                //== Init
+                init: function() {
+                    initDemos();
+                },
+            };
+        }();
+
+        //== Class Initialization
+        jQuery(document).ready(function() {
+            SweetAlert2Demo.init();
         });
     </script>
 @endpush
