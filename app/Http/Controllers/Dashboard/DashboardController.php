c4c4ded062d2de3381ea8519d47b642fa9689f2c
MSI\USER <yogafebrianmaulana12@gmail.com>
2024-03-15 11:14:25 +0700

    dashboard call worker when divisi = IT

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class DashboardController extends Controller
 {
     public function index(){
-
-        return view('dashboard.dashboard');
+        $worker=User::where('divisi','IT')->get();
+        return view('dashboard.dashboard',['worker'=>$worker]);
     }
 }
