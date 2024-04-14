fb15a1c2f8ac4fbb38c7a7c4d39af0b3b95f40ec
suharyadi <suharyadi21@outlook.com>
2024-04-15 00:51:35 +0700

    - kategori + fixing

--- app/Http/Controllers/Api/DashboardHome/DashboardHomeController.php
+++ app/Http/Controllers/Api/DashboardHome/DashboardHomeController.php
@@ -24,7 +24,13 @@ class DashboardHomeController extends Controller
         try {
 
             $totalPersenPengaduan = $this->hitungTotalPersenPengaduan($request);
-            $data = ["totalPersenPengaduan" => $totalPersenPengaduan];
+            $totalPersenPrioritas = $this->hitungTotalPersenPrioritas($request);
+            $totalPersenKategori = $this->hitungTotalKategori($request);
+            $data = [
+                    "totalPersenPengaduan" => $totalPersenPengaduan,
+                    "totalPersenPrioritas" => $totalPersenPrioritas,
+                    "totalPersenKategori" => $totalPersenKategori,
+                    ];
     
             return response(["status"=> "success","message"=> "Data successfully retrieved", "data" => $data], 200);
         } catch (\Exception $e) {
@@ -38,27 +44,21 @@ class DashboardHomeController extends Controller
         $rules = [
             'interval' => 'in:today,month,year',
         ];
-        // Pesan validasi jika diperlukan
         $messages = [
             'interval.in' => 'Interval must be one of: today, month, year.',
         ];
 
-        // Validasi input
         $validator = Validator::make($request->all(), $rules, $messages);
 
-        // Jika validasi gagal, lempar exception
         if ($validator->fails()) {
             throw new \Exception($validator->errors()->first());
         }
 
-        // Set tanggal sesuai dengan interval yang dipilih
         list($currentDate, $previousDate) = $this->getDateRange($request->input('interval'));
 
-        // Hitung jumlah pengaduan
         $currentCount = $this->getPengaduanCount($currentDate, $request->interval);
         $previousCount = $this->getPengaduanCount($previousDate, $request->interval);
 
-        // Hitung persentase peningkatan
         $increasePercentage = $this->calculateIncreasePercentage($currentCount, $previousCount);
 
         return [
@@ -66,6 +66,68 @@ class DashboardHomeController extends Controller
             'previous_count' => $previousCount,
             'increase_percentage' => $increasePercentage,
             'interval' => $request->interval,
+        ];
+    }
+
+    protected function hitungTotalKategori(Request $request) {
+
+        $kategoriId = $request->kategori_id;
+
+        if ($kategoriId == '-') {
+
+            $kategoriId = $this->getRandomkategoriMostMonth();
+
+        }
+
+        list($currentDate, $previousDate) = $this->getDateRange($request->input('intervalKategori'));
+
+        $currentCount = $this->getKategoriCount($currentDate, $request->intervalKategori, $kategoriId);
+        $previousCount = $this->getKategoriCount($previousDate, $request->intervalKategori, $kategoriId);
+        
+        $increasePercentage = $this->calculateIncreasePercentage($currentCount, $previousCount);
+
+        $kategoriName = Pengaduan::with('kategoripengaduan')->where('kategori_pengaduan_id', $kategoriId)->first(); //hanya ambil nama
+
+        return [
+            'current_count' => $kategoriId ? $currentCount : 0,
+            'previous_count' => $kategoriId ? $previousCount : 0,
+            'increase_percentage' => $kategoriId ? $increasePercentage : 0,
+            'interval' => $request->intervalKategori,
+            'kategori' => $kategoriId ? $kategoriName->kategoripengaduan->nama : '-',
+        ];
+    }
+
+
+    protected function hitungTotalPersenPrioritas(Request $request) {
+
+        $rules = [
+            'intervalPrioritas' => 'in:today,month,year',
+            'prioritas' => 'in:tinggi,sedang,rendah',
+        ];
+        $messages = [
+            'intervalPrioritas.in' => 'Interval must be one of: today, month, year.',
+            'prioritas.in' => 'Prioritas must be one of: tinggi, sedang, rendah.',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            throw new \Exception($validator->errors()->first());
+        }
+
+        list($currentDate, $previousDate) = $this->getDateRange($request->input('intervalPrioritas'));
+
+        $currentCount = $this->getPrioritasCount($currentDate, $request->intervalPrioritas, $request->prioritas);
+        $previousCount = $this->getPrioritasCount($previousDate, $request->intervalPrioritas, $request->prioritas);
+
+        $increasePercentage = $this->calculateIncreasePercentage($currentCount, $previousCount);
+
+        return [
+            'current_count' => $currentCount,
+            'previous_count' => $previousCount,
+            'increase_percentage' => $increasePercentage,
+            'prioritas' => $request->prioritas,
+            'interval' => $request->intervalPrioritas,
         ];
     }
 
@@ -106,6 +168,52 @@ class DashboardHomeController extends Controller
         return $currentCount;
     }
 
+    protected function getPrioritasCount($date, $intvl, $prioritas)
+    {   
+
+        if($intvl == "today"){
+            $currentCount = Pengaduan::where('prioritas', $prioritas)->whereDate('tanggal_pelaporan', $date)->count();
+        }else if($intvl == "month"){
+            $currentCount = Pengaduan::where('prioritas', $prioritas)->whereMonth('tanggal_pelaporan', $date)->count();
+        }else if($intvl == "year"){
+            $currentCount = Pengaduan::where('prioritas', $prioritas)->whereYear('tanggal_pelaporan', $date)->count();
+        }
+
+        return $currentCount;
+    }
+
+    protected function getKategoriCount($date, $intvl, $kategoriId)
+    {   
+        if ($intvl == "today") {
+            $currentCount = Pengaduan::where('kategori_pengaduan_id', $kategoriId)->whereDate('tanggal_pelaporan', $date)->count();
+        } else if ($intvl == "month") {
+            $currentCount = Pengaduan::where('kategori_pengaduan_id', $kategoriId)->whereMonth('tanggal_pelaporan', $date)->count();
+        } else if ($intvl == "year") {
+            $currentCount = Pengaduan::where('kategori_pengaduan_id', $kategoriId)->whereYear('tanggal_pelaporan', $date)->count();
+        }
+
+        return $currentCount;
+    }
+
+    protected function getRandomkategoriMostMonth(){
+        $startDate = now()->startOfMonth();
+        $endDate = now()->endOfMonth();
+
+        $jumlahPerKategori = Pengaduan::whereBetween('tanggal_pelaporan', [$startDate, $endDate])
+            ->select('kategori_pengaduan_id', DB::raw('COUNT(*) as total'))
+            ->groupBy('kategori_pengaduan_id')
+            ->orderByDesc('total')
+            ->first();
+
+        if ($jumlahPerKategori) {
+            $kategoriTerbanyakId = $jumlahPerKategori->kategori_pengaduan_id;
+            
+            return $kategoriTerbanyakId;
+        } else {
+            return false;
+        }
+    }
+
     protected function calculateIncreasePercentage($currentCount, $previousCount)
     {
         if ($previousCount > 0) {
@@ -115,6 +223,22 @@ class DashboardHomeController extends Controller
         return 0;
     }
 
+    protected function calculatePriority($currentCount,$previousCount){
 
+        if ($previousCount > 0) {
+            return ($currentCount - $previousCount) / $previousCount * 100;
+        }
+
+        return 0;
+    }
+
+    protected function calculateKategori($currentCount,$previousCount){
+
+        if ($previousCount > 0) {
+            return ($currentCount - $previousCount) / $previousCount * 100;
+        }
+
+        return 0;
+    }
 
 }
